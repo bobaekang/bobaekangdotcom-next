54234579869d76652701ff1556a3add7cb59c588
bobaekang <bkang.dev@gmail.com>
2020-05-14 08:12:15 -0500

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Bobae's place`,
+    author: `Bobae Kang`,
+    description: `Bobae's place to share experience & reflections on software development.`,
+    siteUrl: `https://bobaekang.com/`,
     social: {
-      twitter: `kylemathews`,
+      email: `hello@bobaeknag.com`,
+      github: `bobaekang`,
+      linkedin: `bobaekang`,
     },
   },
   plugins: [
